2e8a20bb0199a796e44b24da5b01171a5c6967e0
Molly Novash <novash@babbage.cs.pdx.edu>
2018-04-22 14:35:14 -0700

    added uproc.h

--- defs.h
+++ defs.h
@@ -118,6 +118,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int             setProcs(uint max, struct uproc * procs);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
